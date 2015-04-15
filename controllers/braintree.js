02b4cfea946788e8eacc29c8e1e84ddaf928bfb1
Adrian Chen <adrianchen@Adrians-MacBook-Pro.local>
2015-04-14 18:28:16 -0600

    made links secure hopefully

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -8,22 +8,22 @@ var brain = function(req, res) {
     privateKey: "de2913aa208fa2e8e410d91587d36d42"
   } );
 
-  gateway.clientToken.generate( {
-    customerId: aCustomerId
-  }, function( err, response ) {
+  gateway.clientToken.generate( {}, function( err, response ) {
     var clientToken = response.clientToken
   } );
 
   app.post( "/purchases", function( req, res ) {
     var nonce = req.body.payment_method_nonce;
-    // Use payment method nonce here
+    gateway.transaction.sale({
+      amount: "0.50",
+      //merchantAccountId: CORRECT_ACCOUNT,
+      paymentMethodNonce: nonce,
+      options: {
+        submitForSettlement: true
+      }
+    }, function (err, result) {
+    });
   } );
-  //
-  //gateway.transaction.sale({
-  //  amount: '10.00',
-  //  paymentMethodNonce: 'nonce-from-the-client',
-  //}, function (err, result) {
-  //});
 }
 
 module.exports = brain;
