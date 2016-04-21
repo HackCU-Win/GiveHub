8bbd809eb0454efe0b2ddc13ecd5a4180c765d29
Brian Newsom <Brian.Newsom@Colorado.edu>
2016-04-21 12:54:03 -0600

    Update mandrill.js

--- controllers/mandrill.js
+++ controllers/mandrill.js
@@ -1,10 +1,10 @@
 var email = function(req, res){
-    var toEmail = "canadianidiot27@gmail.com";
+    var toEmail = "x@gmail.com";
     var toName = "Brian Newsom";
 
 
     var mandrill = require('mandrill-api/mandrill');
-    var mandrill_client = new mandrill.Mandrill('hquHAd89JAE4rzHbtuMtxg');
+    var mandrill_client = new mandrill.Mandrill('x');
 
     var html = "<h5> Please confirm your donation to _ on _</h5>" +
     "<p> Scan the QR code below to confirm. </p>" +
@@ -14,8 +14,8 @@ var email = function(req, res){
     var message = {
         "html": html,
         "subject": "Confirm your donation on _",
-        "from_email": "brian.newsom@colorado.edu",
-        "from_name": "Brian Newsom",
+        "from_email": "x",
+        "from_name": "x",
         "to": [{
                 "email": toEmail,
                 "name": toName,
@@ -32,7 +32,7 @@ var email = function(req, res){
                 "email": "recipient.email@example.com",
                 "status": "sent",
                 "reject_reason": "hard-bounce",
-                "_id": "abc123abc123abc123abc123abc123"
+                "_id": "x"
             }]
         */
     }, function(e) {
